a83e67b7720cfbc78a12100cdd37363e7d5f54da
MikeMoldawsky <16150627+MikeMoldawsky@users.noreply.github.com>
2022-05-03 15:02:26 +0300

    remove config file to dotenv (#22)

--- logic/db/db.js
+++ logic/db/db.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
-const {mongo_connection_string} = require('../../config.json')
 
-const conn = mongoose.createConnection(mongo_connection_string);
+const conn = mongoose.createConnection(process.env.MONGO_URI);
 
 //Bind connection to error event (to get notification of connection errors)
 conn.on('error', console.error.bind(console, 'MongoDB connection error:'));
