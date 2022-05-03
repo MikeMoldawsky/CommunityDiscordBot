d527c8f7b261bd7711ec77af6672e2bfe1945fd4
Mike Moldawsky <your_donotreply_email_id>
2022-05-03 15:23:55 +0300

    remove logs

--- index.js
+++ index.js
@@ -13,11 +13,6 @@ const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
 const client = require('./logic/discord/client')
 
-
-console.log(process.env.DISCORD_TOKEN)
-console.log(process.env.DISCORD_CLIENT_ID)
-console.log(process.env.MONGO_URI)
-
 /**
  * From v13, specifying the intents is compulsory.
  * @type {Object}
--- logic/db/db.js
+++ logic/db/db.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 
-console.log(`In DB: ${process.env.MONGO_URI}`)
 const conn = mongoose.createConnection(process.env.MONGO_URI);
 
 //Bind connection to error event (to get notification of connection errors)
