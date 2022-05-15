0d2ac3ad84e6b697275251ca7d3e1948d5890703
sniirr <sniirr@gmail.com>
2022-05-15 21:03:34 +0300

    match maker - fill rooms with one member

--- logic/tasks/speed-date-match-maker-task.js
+++ logic/tasks/speed-date-match-maker-task.js
@@ -17,40 +17,95 @@ async function createSpeedDatesMatchesInternal(guildBotDoc, forceMatch = false) 
 
 	const guild = await client.guilds.fetch(guildInfo.guildId)
 	const lobbyChannel = await client.channels.fetch(lobby.channelId)
-	const lobbyMembers = lobbyChannel.members.filter(m => !m.user.bot && !_.includes(guildConfig.ignoreUsers, m.user.id))
 
-	if (lobbyMembers.size < 2){
-		console.log(`Match maker - No Enough Members in Lobby`,  { guildInfo, membersCount: lobbyMembers.size});
+	// get available members from lobby and members that are alone in a room if any
+	const aloneMemberDates = getMembersAloneInRoom(dates)
+	const membersAloneInRoom = _.keys(aloneMemberDates)
+	const availableMemberIds = [
+		...getLobbyAvailableMembers(lobbyChannel, guildConfig),
+		...membersAloneInRoom
+	]
+
+	if (availableMemberIds.length < 2){
+		console.log(`Match maker - Not Enough Members in Lobby`,  { guildInfo, membersCount: availableMemberIds.length});
 		return;
 	}
-	const { rooms } = matchRooms(Array.from(lobbyMembers.keys()), datesHistory, config.roomCapacity, forceMatch)
+
+	const { rooms } = matchRooms(availableMemberIds, datesHistory, config.roomCapacity, forceMatch)
 	console.log(`Match maker - Creating ${rooms.length} DATES`, {guildInfo});
 	const maxRoomNum = _.max(_.map(dates, 'number')) || 0
 	const newDates = await Promise.all(
 		rooms.map(async (room, i) => {
-			const roomNumber = maxRoomNum + i + 1;
-			console.log(`Match maker - CREATING DATE.`, {guildInfo, room});
-			const vc = await createSpeedDateVoiceChannelRoom(guild, roomNumber, room);
-			const roomParticipants = room.map((userId) => {
-				const member = guild.members.cache.get(userId)
-				member.voice.setChannel(vc.id)
-				return {id: userId, name: member.user.username}
-			})
+			// check if adding to an existing room
+			const intersection = _.intersection(room, membersAloneInRoom)
+			if (!_.isEmpty(intersection)) {
+				console.log(`Match maker - ADDING MEMBER TO EXISTING DATE.`, {guildInfo, room});
+				const inRoomMember = intersection[0]
+				const { date: joinedRoom } = aloneMemberDates[inRoomMember]
+				const vc = await client.channels.fetch(joinedRoom.voiceChannelId)
+				return {
+					number: joinedRoom.number,
+					participants: addMembersToRoom(guild, room, vc),
+					voiceChannelId: vc.id
+				}
+			}
+			else {
+				const roomNumber = maxRoomNum + i + 1;
+				console.log(`Match maker - CREATING DATE.`, {guildInfo, room});
+				const vc = await createSpeedDateVoiceChannelRoom(guild, roomNumber, room);
+				const roomParticipants = room.map((userId) => {
+					const member = guild.members.cache.get(userId)
+					member.voice.setChannel(vc.id)
+					return {id: userId, name: member.user.username}
+				})
 
-			return {
-				number: roomNumber,
-				participants: roomParticipants,
-				voiceChannelId: vc.id
-			};
+				return {
+					number: roomNumber,
+					participants: roomParticipants,
+					voiceChannelId: vc.id
+				};
+			}
 		})
 	);
 
+	// if members were added to an existing room the room will be in newDates, filter it out from current DB dates
+	const joiningRoomIndices = _.map(aloneMemberDates, ({ index }) => index)
+	const updatedExistingDates = _.filter(dates, (date, i) => !_.includes(joiningRoomIndices, i))
+
 	console.log(`Match maker - Created DATES`, {newDates, guildInfo});
 	const updatedFields = {
-		'activeSession.round.dates': [...dates, ...newDates],
+		'activeSession.round.dates': [...updatedExistingDates, ...newDates],
 	};
 	await findGuildAndUpdate(guildInfo.guildId, updatedFields);
 }
+
+const getLobbyAvailableMembers = (lobbyChannel, guildConfig) => {
+	return Array.from(
+		lobbyChannel.members.filter(m => !m.user.bot && !_.includes(guildConfig.ignoreUsers, m.user.id)).keys()
+	)
+}
+
+const getMembersAloneInRoom = (dates) => {
+	return dates.reduce((aloneMemberDates, date, i) => {
+		const voiceChannel = client.channels.cache.get(date.voiceChannelId)
+		return _.size(voiceChannel.members) === 1
+			? {
+				...aloneMemberDates,
+				[voiceChannel.members.first().user.id]: { date, index: i },
+			}
+			: aloneMemberDates
+	}, {})
+}
+
+const addMembersToRoom = (guild, members, vc) => {
+	return members.map((userId) => {
+		const member = guild.members.cache.get(userId)
+		member.voice.setChannel(vc.id)
+		return {id: userId, name: member.user.username}
+	})
+}
+
+
 
 async function createSpeedDatesMatches(guildBotDoc, forceMatch = false) {
 	try {
