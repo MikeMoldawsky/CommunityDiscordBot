37ac2a4c90d627c4be8b81f28446b38dd8d27087
sniirr <sniirr@gmail.com>
2022-06-07 13:15:01 +0300

    Frontend incomplete (#28)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './assets/connecto.png';
+import video from './assets/connecto-clip.m4v';
+import './App.scss';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faGem, faMountainCity } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <p>
-            Connecto Bot
-          </p>
-        </p>
+      <header>
+        <a target="_blank" href="https://twitter.com/tweezers0xffff">
+          <FontAwesomeIcon icon={faGem} />
+        </a>
+        <a target="_blank" href="https://twitter.com/donaldao0xffff">
+          <FontAwesomeIcon icon={faMountainCity} />
+        </a>
+        <a target="_blank" href="https://twitter.com/connecto0xffff">
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
       </header>
+      <div className="top-box">
+        <div>
+          <img src={logo} className="logo" alt="CONNECTO" />
+          <h1>CONNECTO</h1>
+          <h2>Get to know your community members in a fun and exciting way</h2>
+          <div className="install-box">
+            <a target="_blank" href="https://discord.com/oauth2/authorize?client_id=968173788946636850&permissions=292552720&scope=bot%20applications.commands">
+              <div className="button">INSTALL CONNECTO</div>
+            </a>
+          </div>
+        </div>
+        <div className="video-placeholder">
+          <video src={video} autoPlay muted controls />
+        </div>
+        {/*<div className="install-box">*/}
+        {/*  <div className="button">INSTALL CONNECTO</div>*/}
+        {/*</div>*/}
+      </div>
     </div>
   );
 }
