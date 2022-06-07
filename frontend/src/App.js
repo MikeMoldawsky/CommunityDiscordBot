59e51b9ce36b501a7ce0e3980b414fec16dade40
sniirr <sniirr@gmail.com>
2022-06-07 13:12:41 +0300

    frontend - wip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import logo from './assets/connecto.png';
+import video from './assets/connecto-clip.m4v';
 import './App.scss';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import { faTwitter, faUnity } from '@fortawesome/free-brands-svg-icons'
-import { faGem } from "@fortawesome/free-solid-svg-icons";
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faGem, faMountainCity } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
     <div className="App">
       <header>
-        <a target="_blank" href="https://twitter.com/donaldao0xffff">
-          <FontAwesomeIcon icon={faUnity} />
-        </a>
         <a target="_blank" href="https://twitter.com/tweezers0xffff">
           <FontAwesomeIcon icon={faGem} />
+        </a>
+        <a target="_blank" href="https://twitter.com/donaldao0xffff">
+          <FontAwesomeIcon icon={faMountainCity} />
         </a>
         <a target="_blank" href="https://twitter.com/connecto0xffff">
           <FontAwesomeIcon icon={faTwitter} />
@@ -25,10 +26,14 @@ function App() {
           <h1>CONNECTO</h1>
           <h2>Get to know your community members in a fun and exciting way</h2>
           <div className="install-box">
-            <div className="button">INSTALL CONNECTO</div>
+            <a target="_blank" href="https://discord.com/oauth2/authorize?client_id=968173788946636850&permissions=292552720&scope=bot%20applications.commands">
+              <div className="button">INSTALL CONNECTO</div>
+            </a>
           </div>
         </div>
-        <div className="video-placeholder" />
+        <div className="video-placeholder">
+          <video src={video} autoPlay muted controls />
+        </div>
         {/*<div className="install-box">*/}
         {/*  <div className="button">INSTALL CONNECTO</div>*/}
         {/*</div>*/}
